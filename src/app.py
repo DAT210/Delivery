cf9228697a99bce7c3bba63c2951342a2c28681d
asleboon <asle.berge@gmail.com>
2018-10-09 10:46:04 +0200

    First draft Delivery Ui

--- src/app.py
+++ src/app.py
@@ -1,19 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for
 from flask_cors import CORS
 from flask_restful import Resource, Api
+from flask_bootstrap import Bootstrap
 import json
 import requests
+
+
 app = Flask(__name__)
 api = Api(app)
+# Makes it easy to implement boostrap
+Bootstrap(app)
 # Makes it possible to send POST requests from javascript outside this service.
 CORS(app)
 
+########################### DELIVERY UI #################################
+@app.route('/delivery')
+def index():
+    percent = "50"
+    # TODO hent inn data fra database
+    return render_template("index.html", percent=percent)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World! We have Flask in a Docker container!'
+
+########################### DELIVERY UI #################################
 
 #TODO: Implement functioncalls depending on routes and content of http requests
+
 
 @app.route('/ETA', methods=["POST"])
 def handleETArequest():
@@ -37,4 +48,4 @@ def testingpost():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=)
+    app.run(debug=True)
